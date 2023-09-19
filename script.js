4c5fc913418e03eafce01afe37c6bc4368b03558
Suman Sur <sumansur.cs98@gmail.com>
2023-09-19 20:09:14 +0530

    grid render completed

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
 function renderGrid(size){
+    let width = Math.floor(900/size)-2;
+    let height = width;
+
     for(let i=0;i<size*size;i++){
         const cellElement = document.createElement('div')
         cellElement.classList.add('cell');
+        cellElement.style.height = height+"px";
+        cellElement.style.width = width+"px";
         document.getElementById("gridDiv").append(cellElement);
     }
 }
 
 
-renderGrid(5);
+renderGrid(10);
